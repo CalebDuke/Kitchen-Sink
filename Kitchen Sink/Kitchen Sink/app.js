1b34bd5f0ede0d0273741c77e575cc02bc9a9520
CalebDuke <59664257+CalebDuke@users.noreply.github.com>
2020-01-15 11:12:45 -0600

    Add files via upload

---
+++ Kitchen Sink/Kitchen Sink/app.js
@@ -0,0 +1,71 @@
+// var firstName = ('caleb');
+// const states = (50);
+
+// var a = 5 + 4
+// console.log(a);
+// /*addition function*/
+
+
+// function sayHello() {
+//     alert("Hello World!")
+// }
+// sayHello();
+
+// checkAge("Charles", 65)
+// checkAge("Abby", 27)
+// checkAge("James", 18)
+// checkAge("John", 17)
+
+// var veggies = ['Corn', 'Tomato', 'Avacado', 'Green Beans', 'Beets'];
+// console.log(veggies)
+
+// let person = [
+//     {
+//         name: "Josh",
+//         age: 21
+//     },
+//     {
+//         name: "Caleb",
+//         age: 22
+//     },
+//     //2
+//     {
+//         name: "Dalton",
+//         age: 20
+//     },
+//     //3
+//     {
+//         name: "Bekah",
+//         age: 19
+//     },
+//     //4
+//     {
+//         name: "James",
+//         age: 21
+//     }
+// ]
+
+// function checkAge() {
+//     for (let i = 0; i <person.length; i++) {
+//         if (person[i].age < 21) {
+//             alert("sorry " + person[i].name + ", you arent old enough to view this page!")
+//         }
+//     }
+// }
+
+// checkAge()
+
+var anyWord = "Hello World!"
+
+function getLength(anyWord) {
+    console.log(anyWord.length);
+    var n = anyWord.length;
+    if (n%2 == 0){
+      console.log("The world is an even place!");
+    }else{
+      console.log("The world is an odd place!");
+    }
+  };
+
+  getLength(anyWord);
+
